5edebef79ea735ace453f813c68608654852f95c
Julian Visser <chulio.visser@gmail.com>
2021-11-21 17:39:35 +0100

    add history to headerLogo

--- frontend/src/components/HeaderLogo.jsx
+++ frontend/src/components/HeaderLogo.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+
 import styles from './HeaderLogo.module.css';
 
 import openHack from '../imgs/open_hack_2019-03.svg';
 
 const HeaderLogo = () => {
+  const history = useHistory();
+
   return (
     <div
       className={styles.headerLogo}
