b8cda48fd8c59fe6dbe388e8ac36af0e12f5101b
Julian Visser <chulio.visser@gmail.com>
2021-12-09 10:39:36 +0100

    added mobile view

--- frontend/src/components/CardSelector.jsx
+++ frontend/src/components/CardSelector.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
 import classnames from 'classnames';
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Card from './Card';
 
@@ -9,28 +14,52 @@ import { cards } from '../imgs/cards';
 import styles from './CardSelector.module.css';
 
 const CardSelector = ({ me, setCard, showValue }) => {
+  const matches = useMediaQuery('(min-width:600px)');
+
   const handleSelected = (cardName) => {
+    console.log(cardName);
     setCard(cardName);
   };
 
+  if (matches) {
+    return (
+      <div
+        className={classnames(styles.btnContainer, {
+          [styles.btnsDisabled]: showValue
+        })}>
+        {cards.map((cardName) => (
+          <div
+            key={cardName}
+            className={classnames(styles.card, {
+              [styles.selectedCard]: me?.card === cardName
+            })}
+            onClick={() => handleSelected(cardName)}>
+            <Card cardName={cardName} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div
-      className={classnames(styles.btnContainer, {
-        [styles.btnsDisabled]: showValue,
-      })}
-    >
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      disabled={showValue}
+      value={me?.card || ''}
+      label="Card"
+      onChange={(event) => handleSelected(event.target.value)}>
       {cards.map((cardName) => (
-        <div
-          key={cardName}
-          className={classnames(styles.card, {
-            [styles.selectedCard]: me?.card === cardName,
-          })}
-          onClick={() => handleSelected(cardName)}
-        >
-          <Card cardName={cardName} />
-        </div>
+        <MenuItem value={cardName} key={cardName}>
+          <div className={styles.mobileMenuItem}>
+            <div className={styles.mobileCard}>
+              <Card cardName={cardName} />
+            </div>
+            {cardName}
+          </div>
+        </MenuItem>
       ))}
-    </div>
+    </Select>
   );
 };
 
